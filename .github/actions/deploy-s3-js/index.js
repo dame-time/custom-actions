a30cc43b4d277423e62ad0a832f534389908885d
Davide Paollilo <davide.paolillo@develop-players.com>
2024-05-31 09:04:34 +0200

    Added output to deployment

--- .github/actions/deploy-s3-js/index.js
+++ .github/actions/deploy-s3-js/index.js
@@ -15,6 +15,9 @@ function run() {
 
     // Upload files to S3 bucket
     exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput("website-url", websiteUrl);
 }
 
 run();
